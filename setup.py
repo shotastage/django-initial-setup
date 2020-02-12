b79aedaa0f9b37fe437f4d10d975255b9cd4c0e9
Shota Shimazu <hornet.live.mf@gmail.com>
2020-02-12 20:50:27 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='django-initial-setup',
+    version='0.0.1',
+    description='Django Initial Setup provides inital setup screen developed with Django for first launch.',
+    long_description="""
+    Django Initial Setup provides inital setup screen developed with Django for first launch.
+    """,
+    author='Shota Shimazu',
+    author_email='hornet.live.mf@gmail.com',
+    url='https://github.com/shotastage/django-initial-setup',
+    download_url='https://pypi.python.org/pypi/django-initial-setup',
+    license='MIT',
+    packages=find_packages(exclude=('tests')),
+    install_requires=[
+        'django>=1.0',
+    ],
+    extras_require={
+    },
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Security',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
+    ],
+)
