f96f5b3a89db62522faa6b18a9fcaaa30ecdfa5f
Shota Shimazu <shotastage@users.noreply.github.com>
2021-06-30 12:25:26 +0900

    upgrade dependent packages

--- first_setup/version.py
+++ first_setup/version.py
@@ -1,7 +1,7 @@
 """
 django-initial-setup
-Copyright 2020 Shota Shimazu.
+Copyright 2020-2021 Shota Shimazu.
 This software is licensed under the MIT, see LICENSE for detail.
 """
 
-version = "0.0.9"
+version = "0.1.0"
